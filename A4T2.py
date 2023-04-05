a8779e1b55a74e55e1eccf8ad503701918e4e5ee
wcreaser19 <wcreaser@ualberta.ca>
2023-04-04 19:23:26 -0600

    worked on Task 2 but cant test so don't know if it works or not

--- A4T2.py
+++ A4T2.py
@@ -1,43 +1,43 @@
 """Write a Python application (A4T2.py) which will create another MongoDB database called A4dbEmbed
-containing a single collection called “SongwritersRecordings”. Recordings where all the recordings (in
+containing a single collection called "SongwritersRecordings". Recordings where all the recordings (in
 recordings.json) associated with a songwriter (in songwriter.json) will be embedded along with the
-corresponding songwriters’ documents. Note that songs associated with multiple songwriters should be
+corresponding songwriters' documents. Note that songs associated with multiple songwriters should be
 embedded in each of those songwriter's documents. As an example of an embedded collection,
 assume you have two collections, called Profs and Courses with the possible (partial) schemas:
 Profs:
 [
 {
-prof_id: “luigi1983”,
-prof_email: “luigi@marioland.game”,
-teaches: [ “CMPUT291”, “CMPUT391” ]
+prof_id: "luigi1983",
+prof_email: "luigi@marioland.game",
+teaches: [ "CMPUT291", "CMPUT391" ]
 } ...
 ]
 Courses:
 [
 {
-course_id: “CMPUT291”,
-course_name: “Intro to File and Database Management”,
-level: “undergraduate”
+course_id: "CMPUT291",
+course_name: "Intro to File and Database Management",
+level: "undergraduate"
 },
 {
-course_id: “CMPUT694”,
-course_name: “Spatiotemporal Data Management”,
-level: “graduate”
+course_id: "CMPUT694",
+course_name: "Spatiotemporal Data Management",
+level: "graduate"
 }, ...
 ]
 An embedded document ProfCourses could look like the following:
 [
 {
-prof_id: “luigi1983”,
-prof_email: “luigi@marioland.game”,
+prof_id: "luigi1983",
+prof_email: "luigi@marioland.game",
 teaches: [{
-course_id: “CMPUT291”,
-course_name: “Intro to File and Database Management”,
-level: “undergraduate”
+course_id: "CMPUT291",
+course_name: "Intro to File and Database Management",
+level: "undergraduate"
 }, {
-course_id: “CMPUT694”,
-course_name: “Spatiotemporal Data Management”,
-level: “graduate”
+course_id: "CMPUT694",
+course_name: "Spatiotemporal Data Management",
+level: "graduate"
 }]
 }, ...
 ]
@@ -48,3 +48,24 @@ be running the applications using the following command:
 python3 A4Tx.py <port number>
 For example, to run the application on port 27017, you would use the following command:
 python3 A4Tx.py 27017"""
+
+import json
+import sys
+from pymongo import MongoClient
+
+client = MongoClient('localhost', int(sys.argv[1]))
+db = client["A4dbNorm"]
+dbEmbed = client["A4dbEmbed"]
+songwriters = db["songwriters"]
+recordings = db["recordings"]
+songwriters_recordings = dbEmbed["SongwritersRecordings"]
+
+for songwriter in songwriters.find():
+    songwriter["recordings"] = []
+    for recording in recordings.find({"songwriter_ids": songwriter["songwriter_id"]}):
+        recording.pop("_id")
+        recording.pop("songwriter_ids")
+        songwriter["recordings"].append(recording)
+    songwriters_recordings.insert_one(songwriter)
+
+client.close()
