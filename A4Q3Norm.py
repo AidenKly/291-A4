2c9b67a1c53a94eb5bcf3f90ecc29ff3fc151e72
kchrisalis <60988548+kchrisalis@users.noreply.github.com>
2023-04-05 14:20:11 -0600

    did the norm query
    i think its done

---
+++ A4Q3Norm.py
@@ -0,0 +1,20 @@
+import sys
+from pymongo import MongoClient
+from pprint import pprint 
+
+client = MongoClient('localhost', int(sys.argv[1]))
+db = client["A4dbNorm"]
+songwriters = db["songwriters"]
+recordings = db["recordings"]
+
+selected_songwriters = songwriters.find(None,{"_id": 1, "songwriter_id": 1, "recordings":1})
+
+for songwriter in selected_songwriters:
+    total_length = 0
+    for recording in songwriter["recordings"]:
+        # pprint(recording)
+        selected_recordings = recordings.find({"recording_id": recording},{"length":1})
+        total_length += selected_recordings[0]['length']
+    songwriter["total_length"] = total_length
+    del songwriter["recordings"]
+    pprint(songwriter)
