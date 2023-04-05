bd3607774945ba0f028fca13c6616d178c8deaed
AidenKly <aklymchu@ualberta.ca>
2023-04-05 14:43:32 -0600

    Finished A4Q3Embed.py

---
+++ A4Q3Embed.py
@@ -0,0 +1,14 @@
+"""Find the sum of the length of all recordings for each songwriter along with the songwriter's id."""
+
+from pymongo import MongoClient
+import sys
+from pprint import pprint
+
+client = MongoClient('localhost', int(sys.argv[1]))
+db = client["A4dbEmbed"]
+songwriters_recordings = db["SongwritersRecordings"]
+    
+aggregate = songwriters_recordings.aggregate([{"$unwind" : "$recordings"}, {"$group" : {"_id": "$songwriter_id", "total_length" : {"$sum" : "$recordings.length"}}}])
+
+for entry in aggregate:
+    pprint(entry)
