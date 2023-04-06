3abe38b712b11901bf8e3c134f5dec8f8be87694
AidenKly <aklymchu@ualberta.ca>
2023-04-06 13:04:11 -0600

    removed excess description

--- A4T2.py
+++ A4T2.py
@@ -4,50 +4,7 @@ recordings.json) associated with a songwriter (in songwriter.json) will be embed
 corresponding songwriters' documents. Note that songs associated with multiple songwriters should be
 embedded in each of those songwriter's documents. As an example of an embedded collection,
 assume you have two collections, called Profs and Courses with the possible (partial) schemas:
-Profs:
-[
-{
-prof_id: "luigi1983",
-prof_email: "luigi@marioland.game",
-teaches: [ "CMPUT291", "CMPUT391" ]
-} ...
-]
-Courses:
-[
-{
-course_id: "CMPUT291",
-course_name: "Intro to File and Database Management",
-level: "undergraduate"
-},
-{
-course_id: "CMPUT694",
-course_name: "Spatiotemporal Data Management",
-level: "graduate"
-}, ...
-]
-An embedded document ProfCourses could look like the following:
-[
-{
-prof_id: "luigi1983",
-prof_email: "luigi@marioland.game",
-teaches: [{
-course_id: "CMPUT291",
-course_name: "Intro to File and Database Management",
-level: "undergraduate"
-}, {
-course_id: "CMPUT694",
-course_name: "Spatiotemporal Data Management",
-level: "graduate"
-}]
-}, ...
-]
-The output of this task is a database to be named A4dbEmbed which will reside in the local Mongo
-data repository but it will not be submitted either. You will need to submit only the A4T1.py/A4T2.py file.
-Your applications must accept the port number as a command-line argument. Please note that we will
-be running the applications using the following command:
-python3 A4Tx.py <port number>
-For example, to run the application on port 27017, you would use the following command:
-python3 A4Tx.py 27017"""
+"""
 
 import sys
 from pymongo import MongoClient
